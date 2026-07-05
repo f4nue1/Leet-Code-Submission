5e283f73f618f34ab24e63d0ae437207a7fb74f0
Fanueal <fanuelmarkos1@gmail.com>
2026-07-05 15:40:23 -0500

    File updated at 7/5/2026, 3:40:23 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,7 +1,13 @@
-# Last updated: 7/5/2026, 3:35:31 PM
+# Last updated: 7/5/2026, 3:40:23 PM
 1class Solution:
-2    def getConcatenation(self, nums: List[int]) -> List[int]:
-3        newNums = nums.copy()
-4        for i in range(len(newNums)):
-5            nums.append(newNums[i])
-6        return nums
+2    def shuffle(self, nums: List[int], n: int) -> List[int]:
+3        y = []
+4        oneList = nums[:n]
+5        twoList = nums[n:]
+6        x = 0
+7        for i in range(len(oneList)):
+8            y.append(oneList[i])
+9            y.append(twoList[i])
+10        return y
+11        
+12
