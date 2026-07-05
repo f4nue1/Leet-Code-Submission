2e8e28e825dbab0ceaad15aed76df63c4fe75a55
Fanueal <fanuelmarkos1@gmail.com>
2026-07-05 16:10:02 -0500

    File updated at 7/5/2026, 4:10:02 PM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,18 +1,16 @@
-# Last updated: 7/5/2026, 4:04:54 PM
+# Last updated: 7/5/2026, 4:10:02 PM
 1class Solution:
-2    def moveZeroes(self, nums: List[int]) -> None:
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 3        """
-4        Do not return anything, modify nums in-place instead.
+4        Do not return anything, modify nums1 in-place instead.
 5        """
-6        left = 0
-7        right = 0
-8        while right < len(nums):
-9            if nums[right] != 0:
-10                nums[left] = nums[right]
-11                left += 1
-12            right += 1
-13        while left < len(nums):
-14            nums[left] = 0
-15            left += 1
-16        return nums        
-17        
+6        right = len(nums1) - 1
+7        right1 = len(nums2) - 1
+8        while right >= m:
+9            nums1[right] = nums2[right1]
+10            right -= 1
+11            right1 -= 1
+12        nums1.sort()
+13
+14        
+15        
