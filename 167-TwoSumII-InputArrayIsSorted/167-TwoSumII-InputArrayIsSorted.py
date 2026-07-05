838f209d13ff7b2d6c5ff96705583b52346be738
Fanueal <fanuelmarkos1@gmail.com>
2026-07-05 16:04:54 -0500

    File updated at 7/5/2026, 4:04:54 PM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,13 +1,18 @@
-# Last updated: 7/1/2026, 11:01:25 AM
+# Last updated: 7/5/2026, 4:04:54 PM
 1class Solution:
-2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-3        left = 0
-4        right = len(numbers) - 1
-5
-6        while left < right:
-7            if numbers[left] + numbers[right] > target:
-8                right -= 1
-9            elif numbers[left] + numbers[right] < target:
-10                left += 1
-11            elif numbers[left] + numbers[right] == target:
-12                return[left + 1, right + 1]
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        left = 0
+7        right = 0
+8        while right < len(nums):
+9            if nums[right] != 0:
+10                nums[left] = nums[right]
+11                left += 1
+12            right += 1
+13        while left < len(nums):
+14            nums[left] = 0
+15            left += 1
+16        return nums        
+17        
